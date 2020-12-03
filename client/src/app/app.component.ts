16a0af39ba2024911ea37821b803df44ff80abff
Balineni <hari.balineni.ext@siemens.com>
2020-12-02 22:02:43 -0500

    end of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,25 @@
+import { AccountService } from './_services/account.service';
 
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'The Gardening App';
-users: any;
-  constructor(private http: HttpClient){}
+  users: any;
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-     this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
-
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { AccountService } from './../_services/account.service';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  register() {
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
